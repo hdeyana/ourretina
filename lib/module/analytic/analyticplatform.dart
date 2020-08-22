005c3f83dc3867b19f23d41604bdf35c6c46a0d2
Handi Deyana <handi.deyana@mncgroup.com>
2020-08-23 06:58:14 +0700

    Init Analytic

--- lib/module/analytic/analyticplatform.dart
+++ lib/module/analytic/analyticplatform.dart
@@ -1,1 +1,10 @@
-mixin AnalyticPlatform {}
+import 'package:firebase_analytics/firebase_analytics.dart';
+
+mixin AnalyticPlatform {
+  Future<void> sendfirebaseAnalytic({String event, Map value}) async {
+    final FirebaseAnalytics analytics = FirebaseAnalytics();
+    analytics.logEvent(name: event, parameters: value);
+  }
+
+  Future<void> sendFirestore({String collection, Map value}) async {}
+}
--- lib/module/analytic/events.dart
+++ lib/module/analytic/events.dart
@@ -1,4 +1,23 @@
-mixin OurRetinaEvents {
-  recordSkip() {}
-  recordLastResultCLicked() {}
+import 'package:app/module/analytic/analyticplatform.dart';
+import 'package:app/module/minus/result/data/model/resultitemmodel.dart';
+
+class OurRetinaEvents with AnalyticPlatform {
+  recordSkip() async {
+    await sendfirebaseAnalytic(event: "skipped");
+  }
+
+  recordLastResultCLicked() async {
+    await sendfirebaseAnalytic(event: "last_result_clicked");
+  }
+
+  recordDurationTrial(int second) async {
+    await sendfirebaseAnalytic(event: "last_result_clicked", value: {'duration': second});
+  }
+
+  recordFinishTest(ResultItemModel result) async {
+    final res = result.toMap();
+
+    await sendfirebaseAnalytic(event: "minus_result", value: res);
+    await sendFirestore(collection: "minus_result", value: res);
+  }
 }
